6e829e8a0f2ef1f23ce0f6f681ce3e4247cadd71
Matthew Weitzel <mweitzel@gmail.com>
2013-08-17 11:22:09 -0700

    split thingies into lib and spec

--- spec/thingies_spec.rb
+++ spec/thingies_spec.rb
@@ -1,39 +1,6 @@
-class Device
-  attr_reader :url, :behaviors
-  
-  def initialize(url, behaviors)
-    @device_list ||= []
-    @url = "druby://#{ url }:1234"
-    @behaviors ||= []
-    add_behaviors(behaviors)
-  end
-
-  def listen
-    @device_list
-  end
-
-  def present 
-    true
-  end
-
-  def register_device(url)
-  end
-
-  def to_json
-    behaviors.to_json
-  end
-  
-  private
-
-  def add_behaviors(names)
-    names.each do |name|
-      @behaviors << Behavior.new(name)
-    end
-  end
-end
+require_relative '../lib/thingies'
 
 Behavior = Struct.new(:name)
-
 
 describe Device do
 
