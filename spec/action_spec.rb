95ea7d76733dbd58f80a6881486d17feeda92ed2
Matthew Weitzel <mweitzel@gmail.com>
2013-08-17 12:24:19 -0700

    empty class and pulled out spec_helper

---
+++ spec/action_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Action do
+
+	context "creates appropriate action from passed json object" do
+
+		it "parses json code the way I expect to" do
+			
+		end
+
+	end
+
+
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require_relative '../lib/thingies'
+require_relative '../lib/action'
